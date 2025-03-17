ddd99965699a60e933dfa1658c2cee59a32238c4
Katherine Acosta <katherine.acostabarquero@ucr.ac.cr>
2025-03-16 20:09:11 -0600

    Creación del proyecto y solución del laboratorio 1

---
+++ Laboratorio1_B70047/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio1_B70047.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
