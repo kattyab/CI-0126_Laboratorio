0fead04c17ecb810fd91b192c18a7c9bb6527b3c
Katherine Acosta <katherine.acostabarquero@ucr.ac.cr>
2025-04-11 20:30:10 -0600

    Se implementa API inicial para Paises

---
+++ Lab4/Handlers/PaisesHandler.cs
@@ -0,0 +1,50 @@
+﻿using Lab4.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Lab4.Handlers
+{
+    public class PaisesHandler
+    {
+        private SqlConnection _conexion;
+        private string _rutaConexion;
+        public PaisesHandler()
+        {
+            var builder = WebApplication.CreateBuilder();
+            _rutaConexion = builder.Configuration.GetConnectionString("PaisesContext");
+            _conexion = new SqlConnection(_rutaConexion);
+        }
+        private DataTable CrearTablaConsulta(string consulta)
+        {
+            SqlCommand comandoParaConsulta = new
+            SqlCommand(consulta, _conexion);
+            SqlDataAdapter adaptadorParaTabla = new
+            SqlDataAdapter(comandoParaConsulta);
+            DataTable consultaFormatoTabla = new();
+            _conexion.Open();
+            adaptadorParaTabla.Fill(consultaFormatoTabla);
+            _conexion.Close();
+            return consultaFormatoTabla;
+        }
+        public List<PaisModel> ObtenerPaises()
+        {
+            List<PaisModel> paises = [];
+            string consulta = "SELECT * FROM dbo.Pais ";
+            DataTable tablaResultado = CrearTablaConsulta(consulta);
+            foreach (DataRow columna in tablaResultado.Rows)
+            {
+                paises.Add(
+                new PaisModel
+                {
+                    Id = Convert.ToInt32(columna["Id"]),
+                    Nombre = Convert.ToString(columna["Nombre"]),
+                    Idioma = Convert.ToString(columna["Idioma"]),
+                    Continente = Convert.ToString(columna["Continente"])
+                }
+                );
+            }
+            return paises;
+        }
+
+    }
+}
